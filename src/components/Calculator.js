0b28f2fdffe387a5709b747d3f6785424a943062
rushemaa <rushemaa@gmail.com>
2023-04-27 21:42:33 +0200

    Add events

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,113 +1,220 @@
+import { useState } from 'react';
 import Display from './Display';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [obj, setObj] = useState({ total: null, next: null, operation: null });
+  let disp = 0;
+  if (obj.next) {
+    disp = obj.next;
+  }
+
+  if (obj.total && !obj.next) {
+    disp = obj.total;
+  }
+
   return (
     <div>
       <table>
-        <Display />
+        <Display total={disp} />
         <tbody>
           <tr>
             <td>
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="AC"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 AC
               </button>
             </td>
             <td>
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="+/-"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 +/-
               </button>
             </td>
             <td>
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="%"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 %
               </button>
             </td>
             <td>
-              <button type="button" className="btn btn-orange">
+              <button
+                type="button"
+                value="÷"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-orange"
+              >
                 ÷
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="7"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 7
               </button>
             </td>
             <td>
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="8"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 8
               </button>
             </td>
             <td>
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="9"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 9
               </button>
             </td>
             <td>
-              <button type="button" className="btn btn-orange">
+              <button
+                type="button"
+                value="*"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-orange"
+              >
                 X
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="4"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 4
               </button>
             </td>
             <td>
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="5"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 5
               </button>
             </td>
             <td>
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="6"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 6
               </button>
             </td>
             <td>
-              <button type="button" className="btn btn-orange">
+              <button
+                type="button"
+                value="-"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-orange"
+              >
                 -
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="1"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 1
               </button>
             </td>
             <td>
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="2"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 2
               </button>
             </td>
             <td>
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="3"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 3
               </button>
             </td>
             <td>
-              <button type="button" className="btn btn-orange">
+              <button
+                type="button"
+                value="+"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-orange"
+              >
                 +
               </button>
             </td>
           </tr>
           <tr>
             <td colSpan="2">
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="0"
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 0
               </button>
             </td>
 
             <td>
-              <button type="button" className="btn btn-white">
+              <button
+                type="button"
+                value="."
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-white"
+              >
                 .
               </button>
             </td>
             <td>
-              <button type="button" className="btn btn-orange">
+              <button
+                type="button"
+                value="="
+                onClick={(e) => setObj(calculate(obj, e.target.value))}
+                className="btn btn-orange"
+              >
                 =
               </button>
             </td>
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,12 +1,22 @@
-function Display() {
+import { ReactPropTypes } from 'react';
+
+function Display({ total }) {
   return (
     <thead>
       <tr>
         <th colSpan="4">
-          <input className="display" type="text" disabled value="0" />
+          <input className="display" type="text" disabled value={total} />
         </th>
       </tr>
     </thead>
   );
 }
 export default Display;
+
+Display.defaultProps = {
+  total: '0',
+};
+
+Display.propTypes = {
+  total: ReactPropTypes.string,
+};
